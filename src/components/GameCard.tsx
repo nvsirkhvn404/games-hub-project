73efcb1d9fb95d2ce3c0bae8f327a9a718d887ac
nvsirkhvn404 <nvsirkhvn.study@gmail.com>
2026-01-09 01:46:53 +0530

    Add hover effect to GameCard

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -4,11 +4,12 @@ import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "@/service/image-url";
 import Emoji from "./Emoji";
+import { Link } from "react-router";
 
 export default function GameCard({ game }: { game: Game }) {
 	return (
 		<>
-			<Card>
+			<Card className="transition-all ease-out duration-200 hover:scale-105 hover:shadow-2xl">
 				<CardHeader>
 					<img src={getCroppedImageUrl(game.background_image)} alt={game.name} />
 				</CardHeader>
@@ -19,7 +20,7 @@ export default function GameCard({ game }: { game: Game }) {
 						/>
 					<CriticScore score={game.metacritic} />
 					</div>
-					{game.name}
+					<Link to={`/games/${game.id}`}>{game.name}</Link>
 					<Emoji rating={game.rating_top} />
 				</CardContent>
 			</Card>
