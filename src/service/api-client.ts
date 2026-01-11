43059d67238c32d3e37a4426c01c91c1c8732833
nvsirkhvn404 <nvsirkhvn.study@gmail.com>
2026-01-11 07:22:37 +0530

    Store API key in env

--- src/service/api-client.ts
+++ src/service/api-client.ts
@@ -9,7 +9,7 @@ export interface FetchResponse<T> {
 const axiosInstance = axios.create({
 	baseURL: "https://api.rawg.io/api",
 	params: {
-		key: "80b4115144cf429c9be9cd6fd1b71120",
+		key: import.meta.env.VITE_API_KEY,
   },
 });
 
