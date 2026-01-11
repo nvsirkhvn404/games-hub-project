48ee574e2fd4562bea05e260ff7ab7597de0c25c
nvsirkhvn404 <nvsirkhvn.study@gmail.com>
2026-01-11 06:58:39 +0530

    Add tooltip in emoji

--- src/components/Emoji.tsx
+++ src/components/Emoji.tsx
@@ -2,12 +2,16 @@ import meh from "../assets/meh.webp";
 import thumbsUp from "../assets/thumbs-up.webp";
 import bullsEye from "../assets/bulls-eye.webp";
 import type { ImgHTMLAttributes } from "react";
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipTrigger,
+} from "@/components/ui/tooltip";
 
-const Emoji = ({ rating, size=8 }: { rating: number; size: number }) => {
-
+const Emoji = ({ rating, size = 8 }: { rating: number; size: number }) => {
 	if (rating < 3) return null;
 
-	const emojiMap: {[key: number]: ImgHTMLAttributes<HTMLImageElement>} = {
+	const emojiMap: { [key: number]: ImgHTMLAttributes<HTMLImageElement> } = {
 		3: { src: meh, alt: "meh" },
 		4: { src: thumbsUp, alt: "recommended" },
 		5: { src: bullsEye, alt: "exceptional" },
@@ -16,7 +20,14 @@ const Emoji = ({ rating, size=8 }: { rating: number; size: number }) => {
 	const emoji = emojiMap[rating];
 	if (!emoji) return null;
 
-	return <img {...emoji} className={`size-${size} mt-1` }/>;
+	return (
+		<Tooltip>
+			<TooltipTrigger>
+				<img {...emoji} className={`size-${size} mt-1`} />
+			</TooltipTrigger>
+			<TooltipContent className="text-xl font-bold">{emoji.alt}</TooltipContent>
+		</Tooltip>
+	);
 };
 
 export default Emoji;
